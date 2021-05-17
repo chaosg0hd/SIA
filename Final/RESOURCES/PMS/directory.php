eb441ba63e372c64451660a0a2b9d06517d9e867
chaosg0hd <77721525+chaosg0hd@users.noreply.github.com>
2021-05-18 02:23:46 +0800

    1

--- Final/RESOURCES/PMS/directory.php
+++
@@ -1,6 +0,0 @@
-
-<a href="index.php?sys=PMS&page=home" id ="sidebar-home" class ="nav-item"><span class ='icon-field'><i class="fa fa-home"></i></span> Home</a>
-<a href="index.php?sys=PMS&page=payroll" id ="sidebar-payroll" class ="nav-item"><span class ='icon-field'><i class="fa fa-home"></i></span> Payroll</a>
-<a href="index.php?sys=PMS&page=home" id ="sidebar-home" class ="nav-item"><span class ='icon-field'><i class="fa fa-home"></i></span> Employee</a>
-<a href="index.php?sys=PMS&page=home" id ="sidebar-home" class ="nav-item"><span class ='icon-field'><i class="fa fa-home"></i></span> Home</a>
-<a href="index.php?sys=PMS&page=home" id ="sidebar-home" class ="nav-item"><span class ='icon-field'><i class="fa fa-home"></i></span> Home</a>
--- Final/RESOURCES/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$conn = new mysqli('localhost', 'root', '', 'cocolime_db-1');
-
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-?>
